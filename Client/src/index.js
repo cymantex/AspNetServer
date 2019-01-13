831cad1f7c2bf6d64444b6e731f5bc87b1a5b538
dv16sen <dv16sen@cs.umu.se>
2019-01-13 22:28:28 +0100

    Minified client to focus on the server side code

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import {render} from "react-dom";
-import {Provider} from "react-redux";
-import {initStore} from "./redux/store";
-import "./sass/style.scss";
-import {Router} from "./Router";
-import $ from "jquery";
 
 render(
-    <Provider store={initStore()}>
-        <Router/>
-    </Provider>,
-    $("#root")[0]
+    <h1>App</h1>,
+    document.getElementById("root")
 );
--- Client/src/pages/HomePage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Page} from "../site-components/views/Page";
-
-export const HomePage = () => (
-    <Page id="home-page">
-        
-    </Page>
-);
